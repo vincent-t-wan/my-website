0de628acf1ea68bb7f461bfa750ec8eef24fc4f0
unknown <wanv2002@gmail.com>
2022-04-12 18:55:10 -0700

    tailwind

--- src/components/About.js
+++ src/components/About.js
@@ -11,20 +11,18 @@ export default function About() {
                 apps.
               </h1>
               <p className="mb-8 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                Laborum, voluptas natus?
+                Ever since ... (insert cliche paragraph here)
               </p>
               <div className="flex justify-center">
                 <a
                   href="#contact"
                   className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                  Work With Me
+                  Contacts
                 </a>
                 <a
                   href="#projects"
                   className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                  See My Past Work
+                  My Projects
                 </a>
               </div>
             </div>
