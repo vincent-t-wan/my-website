23b022639ee04b9602a39b9fbc803829cadd7794
vincent-t-wan <wanv2002@gmail.com>
2022-04-12 19:32:29 -0700

    styles?

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,13 @@ export default function About() {
         <section id="about">
 
             <div className= "greeting">
-                <span id="sentence" class="sentence"></span>
+                <span id="sentence" class="sentence">std::cout &lt;&lt; Hello! I'm Vincent. &lt;&lt; std::endl;</span>
                 <span class="input-line"></span>
             </div>
 
           <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                std::cout &lt&lt Hello! I'm Vincent. &lt&lt std::endl;
                 <br className="hidden lg:inline-block" />I love to build amazing
                 apps.
               </h1>
