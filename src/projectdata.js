d20d73da38fb893b541846cd866f5c9538a85831
Vincent Wan <77179747+vincent-t-wan@users.noreply.github.com>
2023-09-03 13:49:36 -0400

    changed phrasing of desc

--- src/projectdata.js
+++ src/projectdata.js
@@ -8,7 +8,7 @@ export const projectdata = [
       title: "Grade Analyzer Discord Bot",
       tools: "JavaScript, Python",
       description:
-        ["A discord bot that provides functionalities for students and their course grades, such as predicting whether a student should take a certain course, displaying graphs of course grades, and grade comparison with other students.", "Implemented a database server on my Raspberry Pi to store student and grade information.", "Trains a neural network with BERT encodings of all the data to make predictions."],
+        ["A discord bot that provides functionalities for students and their course grades, such as predicting whether a student should take a certain course, displaying graphs of course grades, and comparing grades with other students.", "Implemented a database server on my Raspberry Pi to store student and grade information.", "Trains a neural network with BERT encodings of course names to make predictions."],
       image: "/assets/grade_analyzer.png",
       link: "https://github.com/vincent-t-wan/grade-analyzer",
       font: '1.1vw',
