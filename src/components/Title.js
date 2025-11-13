fb3c0fec37f3254d1bb5036fb17d1361018636a5
Vincent Wan <77179747+vincent-t-wan@users.noreply.github.com>
2025-11-12 23:12:22 -0500

    update infos

--- src/components/Title.js
+++ src/components/Title.js
@@ -32,7 +32,7 @@ class Title extends Component {
         </div>
         <div className="greetingsub">
           <p>
-            A 4B Computer Science student at the University of Waterloo.
+            A developer, tech enthusiast, and math enjoyer
           </p>
           <h6>Scroll down to learn more about me!</h6>
           <p className="arrow-container"><i class="arrow down"></i></p>
